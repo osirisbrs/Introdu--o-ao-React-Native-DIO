8620cfe36aea466887378167ab1863b10261f78b
uDiog <Diogopq2010@gmail.com>
2022-06-30 00:20:19 -0300

    -up name

--- App.js
+++ App.js
@@ -1,20 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  View,
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  Button,
+  Pressable,
+  Linking,
+} from "react-native";
 
-export default function App() {
+const colorGitHub = "gray";
+const imageProfileLinkein =
+  "https://media-exp1.licdn.com/dms/image/C4E03AQENKTVAxcJYYw/profile-displayphoto-shrink_800_800/0/1654217113039?e=1661990400&v=beta&t=EOfgLxrYZSJGbdSWQYXUXcmLjy_9llDFLFbAQm2HSUo";
+const urltoGitHub = "https://github.com/osirisbrs";
+const urltoLinkeId = "https://www.linkedin.com/in/lclucascouto/";
+const App = () => {
+  const handlePressGoToGithub = async () => {
+    const res = await Linking.canOpenURL(urltoGitHub);
+    if (res) {
+      await Linking.openURL(urltoGitHub);
+    }
+  };
+  const handlePressGoToLinkeId = async () => {
+    const res = await Linking.canOpenURL(urltoLinkeId);
+    if (res) {
+      await Linking.openURL(urltoLinkeId);
+    }
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={style.container}>
+      <StatusBar backgroundColor={colorGitHub} barStyle="light-content" />
+      <View style={style.content}>
+        <Image style={style.avatar} source={{ uri: imageProfileLinkein }} />
+        <Text style={[style.defaultText, style.name]}>Lucas Couto</Text>
+        <Text style={[style.defaultText, style.descrition]}>
+          Software Engineering | Backend | SRE | DevOps | NODEJS |
+        </Text>
+        <Pressable onPress={handlePressGoToLinkeId}>
+          <View style={style.button}>
+            <Text>Open in LinkedIn!</Text>
+          </View>
+          </Pressable>
+          <Pressable onPress={handlePressGoToGithub}>
+          <View style={style.button}>
+            <Text>Open in GitHub!</Text>
+          </View>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
+export default App;
+
+const style = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colorGitHub,
+    flex: 1, //expandir para tela inteira
+    justifyContent: "center",
+  },
+  content: {
+    alignItems: "center",
+    padding: 20,
+  },
+  avatar: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+  },
+  defaultText: {
+    color: "white",
+  },
+  name: {
+    marginTop: 20,
+    fontWeight: "bold",
+    fontSize: 24,
+  },
+  descrition: {
+    fontWeight: "bold",
+    fontSize: 14,
+  },
+  button: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 20,
+    marginTop: 15,
   },
 });
